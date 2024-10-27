7910fe9f17cc04fbcdc9f95fd5334323c8e77146
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-10-27 14:10:06 +0000

    Exibe os dados da turma selecionada.

--- src/app/components/turma/turma.component.ts
+++ src/app/components/turma/turma.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Turma } from '../../models/turma';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-turma',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './turma.component.html',
   styleUrl: './turma.component.css'
 })
