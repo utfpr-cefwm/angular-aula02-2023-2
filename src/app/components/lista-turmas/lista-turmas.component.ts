2bd6a0a505420a24cb12e86130199465963df765
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-10-27 13:26:49 +0000

    Exibe os botões de seleção de turma.

--- src/app/components/lista-turmas/lista-turmas.component.ts
+++ src/app/components/lista-turmas/lista-turmas.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TurmaService } from '../../services/turma.service';
 
 @Component({
   selector: 'app-lista-turmas',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './lista-turmas.component.html',
   styleUrl: './lista-turmas.component.css'
 })
 export class ListaTurmasComponent {
-
+  public turmaService = inject(TurmaService);
 }
